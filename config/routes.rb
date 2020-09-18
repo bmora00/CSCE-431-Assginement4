744cbb192dd1d95903ecc7866cf7731a17b83f5b
bmora00 <bmora00@tamu.edu>
2020-09-18 17:01:05 -0500

    transferring files from other github

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  root 'home#index'
+  get 'home/index'
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+
+  
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/new'
+  # get 'books/edit'
+  # get 'books/delete'
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
